905498c63807eefe2899714707b3b3b4e127279f
Gnlow <gnlow@pm.me>
2026-08-07 16:15:20 +0900

    test simple

--- test/co.test.ts
+++ test/co.test.ts
@@ -1,11 +1,12 @@
-import { assert, assertEquals, assertThrows, assertLess } from "https://esm.sh/jsr/@std/assert@1.0.19"
-import { Dist, Z } from "../mod.ts"
+import { assertAlmostEquals } from "https://esm.sh/jsr/@std/assert@1.0.19"
+import { Dist, Z, arr } from "../mod.ts"
+import * as m from "https://gnlow.dev/@learn/moment@0.1.0"
 
-Deno.test("co", () => {
+Deno.test("simple", () => {
     const a = Dist.n().withKey("a")
     const b = Dist.n().withKey("c").co(a, 0.9)
-    const c = a.destine(0 as Z).cross(b)
-    console.log(c.ctx)
-    console.log(c.pick(0.123))
+        .apriori(a, 1 as Z)
+    const samples = arr(10000).map(() => b.pick(Math.random()))
+    assertAlmostEquals(m.mean(samples), 0.9, 0.01)
+    assertAlmostEquals(m.variance(samples), 0.19, 0.01)
 })
-
