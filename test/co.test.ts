34d2f2c230bb49dac7d413137218719ead7c5766
Gnlow <gnlow@pm.me>
2026-08-08 14:39:57 +0900

    idk how to do. maybe this is best?

--- test/co.test.ts
+++ test/co.test.ts
@@ -2,7 +2,7 @@ import { assertAlmostEquals } from "https://esm.sh/jsr/@std/assert@1.0.19"
 import { Dist, Z, arr } from "../mod.ts"
 import * as m from "https://gnlow.dev/@learn/moment@0.1.0"
 import { pearson } from "https://gnlow.dev/@learn/pearson@0.1.2"
-/*
+
 Deno.test("simple apriori", () => {
     const a = Dist.n()
     const b = Dist.n().co(a, 0.9)
@@ -24,7 +24,7 @@ Deno.test("pearson", () => {
     const c = Dist.cross([a, b])
     const samples = arr(10000).map(() => c.pick(Math.random()+""))
     assertAlmostEquals(pearson(samples), 0.8, 0.01)
-})*/
+})
 Deno.test("path", () => {
     const a = Dist.n()
     const b = Dist.n().co(a, 0.7)
@@ -39,5 +39,5 @@ Deno.test("path", () => {
         d: d.key,
     })
     const samples = arr(10000).map(() => Dist.cross([a, d]).pick(Math.random()+""))
-    console.log(pearson(samples))
+    assertAlmostEquals(m.variance(samples.map(s => s[1])), 1, 0.1)
 })
