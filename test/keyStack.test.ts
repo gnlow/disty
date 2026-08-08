fd300df0dc259bf6a58b443a1f5ab6d9beb1942e
Gnlow <gnlow@pm.me>
2026-08-08 10:39:05 +0900

    moreTest

--- test/keyStack.test.ts
+++ test/keyStack.test.ts
@@ -1,9 +1,16 @@
-import { assertEquals } from "https://esm.sh/jsr/@std/assert@1.0.19"
+import { assertEquals, assertNotEquals } from "https://esm.sh/jsr/@std/assert@1.0.19"
 import { Dist } from "../mod.ts"
 
 Deno.test("keyStack", () => {
     const d = Dist.f(s => s)
         .flatMap(s1 => Dist.f(s2 => JSON.stringify([s1, s2])))
-    console.log(d.key)
+    const d2 = Dist.f(s => s)
+        .flatMap(s1 => Dist.f(s2 => JSON.stringify([s1, s2])))
+    
     assertEquals(d.pick(0.123), d.pick(0.123))
+    assertNotEquals(d.pick(0.123), d2.pick(0.123))
+    assertNotEquals(
+        Dist.f(s => s).pick(0.123),
+        Dist.f(s => s).pick(0.123),
+    )
 })
