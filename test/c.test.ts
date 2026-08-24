263b312b73ca3e8edcbb468ec69878db0d988ca6
Gnlow <gnlow@pm.me>
2026-08-24 12:09:20 +0900

    c test

--- test/c.test.ts
+++ test/c.test.ts
@@ -3,11 +3,11 @@ import { Dist } from "../mod.ts"
 
 Deno.test("c", () => {
     const human = Dist.cross({
-        name_: Dist.u(["James"]),
+        name: Dist.u(["James"]),
     })
     class Human extends human.c() {
         greet() {
-            console.log(`Hello, my name is ${this.name_}`)
+            console.log(`Hello, my name is ${this.name}`)
         }
     }
     const human1 = new Human("hi")
